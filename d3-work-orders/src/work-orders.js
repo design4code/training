02d9d0527f09c28dd35500085b6b5727fdc0e594
Tyrone <tyrone.gower@pragmaworld.net>
2020-02-04 06:44:44 +0200

    update

---
+++ d3-work-orders/src/work-orders.js
@@ -0,0 +1,22 @@
+const workOrders = [
+  {
+    "name": "Tyrone",
+    "start-date": 20190128,
+    "end-date": 20190228
+  },
+  {
+    "name": "Bob",
+    "start-date": 20190214,
+    "end-date": 20190222
+  },
+  {
+    "name": "Jean",
+    "start-date": 20190314,
+    "end-date": 20190422
+  },
+  {
+    "name": "Sue",
+    "start-date": 20190529,
+    "end-date": 20190620
+  }
+]
